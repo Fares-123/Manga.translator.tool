44118d1db23f851610873fa12ca9665f5c188f7a
Fares-123 <77728948+Fares-123@users.noreply.github.com>
2025-01-09 01:38:59 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,4 @@
-function startMining() {
-    fetch('/mine', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: 'data=miningData'
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('balance').innerText = data.balance;
-        alert("Mining Successful!");
-    });
-}
-
-document.getElementById('transferForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const address = document.getElementById('address').value;
-    const amount = document.getElementById('amount').value;
-
-    fetch('/transfer', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: `address=${address}&amount=${amount}`
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.status === 'success') {
-            document.getElementById('transferMessage').innerText = data.message;
-            document.getElementById('balance').innerText = data.balance;
-        } else {
-            document.getElementById('transferMessage').innerText = data.message;
-        }
-    });
-});
+window.onload = function() {
+    console.log("صفحة التعدين تم تحميلها بنجاح.");
+    // هنا يمكنك إضافة وظائف أخرى لتفاعل الصفحة
+};
