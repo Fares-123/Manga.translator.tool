d16ed0674c1e0fb7e661e72818e97a38fddf8839
Fares-123 <77728948+Fares-123@users.noreply.github.com>
2025-01-09 15:11:34 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,9 @@
 setInterval(function() {
     fetch('/latest-block')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('hash').innerText = 'Hash: ' + data.hash;
-            document.getElementById('nonce').innerText = 'Nonce: ' + data.nonce;
-            document.getElementById('time').innerText = 'Time Taken: ' + data.time_taken + ' seconds';
-        });
-}, 1000);
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("nonce").textContent = data.nonce;
+        document.getElementById("hash").textContent = data.hash;
+        document.getElementById("timeTaken").textContent = data.time_taken + " ثواني";
+    });
+}, 2500);  // تحديث البيانات كل ثانيتين ونصف
