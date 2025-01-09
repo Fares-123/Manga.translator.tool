10b704ec3887c8b3eab61920441f7e18102c026d
Fares-123 <77728948+Fares-123@users.noreply.github.com>
2025-01-09 15:58:54 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,10 @@
-setInterval(function() {
-    fetch('/latest-block')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("nonce").textContent = data.nonce;
-        document.getElementById("hash").textContent = data.hash;
-        document.getElementById("timeTaken").textContent = data.time_taken + " ثواني";
+// main.js
+function fetchLatestBlock() {
+    $.get("/latest-block", function(data) {
+        $('#nonce').text(data.nonce);
+        $('#hash').text(data.hash);
+        $('#time_taken').text(data.time_taken);
     });
-}, 2500);  // تحديث البيانات كل ثانيتين ونصف
+}
+
+setInterval(fetchLatestBlock, 5000); // Update every 5 seconds
